000495691e626393d273bdfb65313780c9a2f3d1
schweigenderFlugel <facundoezcastro@gmail.com>
2023-06-24 19:24:13 -0300

    introducción a express

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')))
+
+app.get('/', (req, res, next) => {
+    res.sendFile('./views/index.html', { root: __dirname })
+});
+
+app.get('/new-page(.html)?', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html' ))
+});
+
+app.get('/old-page(.html)?', (req, res, next) => {
+    res.redirect(301,'/new-page.html');
+});
+
+app.get('/*', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html' ))
+})
+
+app.listen(3000, () => {
+    console.log('Esto escuchando por el puerto 3000');
+})
